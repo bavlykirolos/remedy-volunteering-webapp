51bceec931aea36a3719ae0bb60a7e71f6853e66
Bavly <ge43kob@mytum.de>
2026-01-16 15:03:52 +0100

    second commit

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+
 import React, { useState, useEffect, useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import FullCalendar from '@fullcalendar/react';
